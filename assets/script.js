29d3325f320c59cd75911e6ad1997c28d922d101
Renilson Medeiros <50431134+malcoon@users.noreply.github.com>
2021-12-27 21:54:33 -0300

    Responsividade Finalizada

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,16 @@
+// ScroolReveal
+
 const sr = ScrollReveal({
     distance: "25px",
     duration: 2000,
   });
 
+  // Header
   ScrollReveal().reveal(".logo", { delay: 150, origin: "left" });
   ScrollReveal().reveal(".navbar", { delay: 150, origin: "right" });
+
+  // Navbar mobile
+  ScrollReveal().reveal(".navbar-hamburger", { delay: 100, origin: "right" });
 
   // Primary page Home / Login
   ScrollReveal().reveal(".primary-content", { delay: 150, origin: "top" });
@@ -25,3 +31,19 @@ const sr = ScrollReveal({
   // Footer
   ScrollReveal().reveal(".copyright", { delay: 100, origin: "bottom" });
   ScrollReveal().reveal(".terms", { delay: 100, origin: "bottom" });
+
+
+// Navbar Mobile
+const hamburger = document.querySelector(".navbar-hamburger");
+const navNavbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navNavbar.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navNavbar.classList.remove("active");
+}))
+
